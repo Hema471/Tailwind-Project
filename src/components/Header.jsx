7c5111d138464c141e4864592b403fa51c25f461
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-04 20:11:57 +0200

    Responsive Navbar Created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       if (window.scrollY > 100) {
         headerRef.current.style.background = "#0c1524";
         headerRef.current.style.padding = "20px 0";
-      }else if (window.scrollY < 100){
+      } else if (window.scrollY < 100) {
         headerRef.current.style.background = "transparent";
         headerRef.current.style.padding = "60px 0";
       }
@@ -19,14 +19,14 @@ const Header = () => {
       ref={headerRef}
       className="pt-[60px] fixed  w-full top-0 left-0 z-50 transition-all duration-200"
     >
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center flex-col sm:flex-row gap-[30px] sm:gap-0">
         <a href="/">
           <img src="/src/assets/logo.svg" alt="Logo" />
         </a>
         <nav>
-          <ul className="flex gap-5">
+          <ul className="flex gap-7">
             {links.map((x) => (
-              <li key={x} className="">
+              <li key={x}>
                 <a
                   href={`/${x.toLowerCase()}`}
                   className="text-white opacity-[0.9] hover:underline hover:opacity-[1] transition-opacity duration-200"
