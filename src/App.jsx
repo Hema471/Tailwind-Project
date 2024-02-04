4802b82bc243b523ffd0e15d856dad11d7317562
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-04 13:02:13 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <span className="bg-black">
+      <h1 className="font-bold text-red-700 bg-green-500">Hello world!</h1>
+    </span>
+  );
+};
+
+export default App;
