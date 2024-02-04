fd4c53de2b7acc917376cf0f34708f871ca6abf6
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-04 17:54:29 +0200

    Navbar created with flexbox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const [links, setLinks] = useState(["Features", "Team", "Signin"]);
+ 
   return (
-    <header className="container">
-      <a href="/">
-        <img src="/src/assets/logo.svg" alt="Logo" />
-      </a>
-      <nav>
-        <ul>
-          {links.map((x) => (
-            <li key={x}>
-              <a href={`/${x.toLowerCase()}`} className="text-white opacity-[0.9] hover:underline hover:opacity-[1] transition-opacity duration-200">
-                {x}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header className="pt-[60px] fixed  w-full top-0 left-0 z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/">
+          <img src="/src/assets/logo.svg" alt="Logo" />
+        </a>
+        <nav>
+          <ul className="flex gap-5">
+            {links.map((x) => (
+              <li key={x} className="">
+                <a
+                  href={`/${x.toLowerCase()}`}
+                  className="text-white opacity-[0.9] hover:underline hover:opacity-[1] transition-opacity duration-200"
+                >
+                  {x}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
