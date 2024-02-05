a927985377ed2aa3314534d800f4ac49f337f6b6
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-05 14:47:37 +0200

    Button created in landing.jsx

--- src/components/Lainding.jsx
+++ src/components/Lainding.jsx
@@ -2,22 +2,30 @@ import React from "react";
 
 const Lainding = () => {
   return (
-    <section className="container">
-      <div className="element-centeredByFlex flex-col pt-[200px]">
-        <div>
-          <img src="src/assets/illustration-intro.png" alt="Landing-Image" />
-        </div>
-        <div className="text-white text-center ">
-          <h1 className="text-3xl font-bold mb-5">
-            All your source files in one secure Location,
-            <br />
-            accessible anywhere
-          </h1>
-          <p className="text-lg  px-[15px] md:w-[600px] mx-auto max-w-full ">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
-            laudantium quas quam accusamus officiis delectus esse magnam
-            quibusdam ex similique libero, quisquam distinctio
-          </p>
+    <section className="bg-[#1c2230]  ">
+      <div className="container">
+        <div className="element-centeredByFlex flex-col pt-[200px]">
+          <div>
+            <img src="src/assets/illustration-intro.png" alt="Landing-Image" />
+          </div>
+          <div className="text-white text-center ">
+            <h1 className="text-3xl font-bold mb-5">
+              All your source files in one secure Location,
+              <br />
+              accessible anywhere
+            </h1>
+            <p className="text-lg  px-[15px] md:w-[600px] mx-auto mb-[30px]">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptates laudantium quas quam accusamus officiis delectus esse
+              magnam quibusdam ex similique libero, quisquam distinctio
+            </p>
+          </div>
+          <a
+            href="/"
+            className="btn element-centeredByFlex w-[280px] h-[60px] rounded-[30px] text-white font-medium text-2xl"
+          >
+            Get Started
+          </a>
         </div>
       </div>
     </section>
