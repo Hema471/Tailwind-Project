ed08d8331618d47bb81cadc4ed97385169154318
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-13 19:56:06 +0200

    Footer Cont...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { CiTwitter } from "react-icons/ci";
 const Footer = () => {
   const [links, setLinks] = useState([
     "About Us",
@@ -11,40 +14,43 @@ const Footer = () => {
     "PRivacy",
   ]);
   return (
-    <section className="container">
-      <div className="1">
-        <a href="/">
-          <img src="/src/assets/logo.svg" alt="Logo" />
-        </a>
-        <a href="/">
-          <img src="/src/assets/icon-location.svg" alt="icon-location" />
-        </a>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolor
-          voluptate, laudantium doloribus ullam, in repudiandae nemo corporis
-          at, praesentium illum veritatis voluptas odit iste! Deleniti corrupti
-          consectetur maiores itaque.
-        </p>
-      </div>
-      <div className="2">
-        <a href="/">
-          +012321542
-          <img src="/src/assets/icon-phone.svg" alt="icon-phone" />
-        </a>
-        <a href="/">
-          example@fylo.com
-          <img src="/src/assets/icon-email.svg" alt="icon-phone" />
-        </a>
-      </div>
-      <div className="3">
-        <ul>
-          {links.map((link) => (
-            <li key={link}>{link}</li>
-          ))}
-        </ul>
-      </div>
-      <div className="4">
-        {/* 3 Icons From React Icons */}
+    <section className="bg-[#0d1424]">
+      <div className="container w-full gap-20 text-white grid md:grid-cols-4 md:mt-[-170px] md:pt-[350px] md:pb-[100px]">
+        <div className="1">
+          <a href="/">
+            <img src="/src/assets/logo.svg" alt="Logo" />
+          </a>
+          <a href="/">
+            <img src="/src/assets/icon-location.svg" alt="icon-location" />
+          </a>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolor
+            voluptate, laudantium doloribus ullam, in repudiandae nemo corporis
+          </p>
+        </div>
+        <div className="2 flex justify-between">
+          <a href="/">
+            <img src="/src/assets/icon-phone.svg" alt="icon-phone" />
+          </a>
+          <p>+012321542</p>
+          <a href="/">
+            <img src="/src/assets/icon-email.svg" alt="icon-phone" />
+          </a>
+          <p> example@fylo.com</p>
+        </div>
+        <div className="3">
+          <ul>
+            {links.map((link) => (
+              <li key={link}>{link}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="4">
+          {/* 3 Icons From React Icons */}
+          <FaFacebookF />
+          <CiTwitter />
+          <FaInstagram />
+        </div>
       </div>
     </section>
   );
